372ad3ffabf11bef65eda453fe1c08038671f5fa
EVargasBreick <114616038+EVargasBreick@users.noreply.github.com>
2023-05-26 19:10:14 -0400

    Delete emizor.js

--- routes/emizor.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../controllers/emizor_controller");
-router.post("/facturas/enviar", controller.sendInvoiceEmizor);
-module.exports = router;
