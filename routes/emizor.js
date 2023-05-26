229c27c9315906ae244455a1940cbd285fd46cac
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-26 19:04:33 -0400

    fix merge

--- routes/emizor.js
+++ routes/emizor.js
@@ -3,4 +3,5 @@ const router = express.Router();
 
 const controller = require("../controllers/emizor_controller");
 router.post("/facturas/enviar", controller.sendInvoiceEmizor);
+router.post("/emizor/oauth/token", controller.postOauthToken);
 module.exports = router;
