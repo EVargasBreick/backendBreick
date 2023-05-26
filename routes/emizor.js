c131f619c6e661884213821fdd107a64fd5b1c52
EVargasBreick <114616038+EVargasBreick@users.noreply.github.com>
2023-05-26 19:15:46 -0400

    emizor fiiles deleted

--- routes/emizor.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../controllers/emizor_controller");
-router.post("/facturas/enviar", controller.sendInvoiceEmizor);
-router.post("/emizor/oauth/token", controller.postOauthToken);
-module.exports = router;
