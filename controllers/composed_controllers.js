2158ec809772aa8ddc4d32ffa866e1ba98776867
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-07-07 18:32:38 -0400

    add retries when emizor is not up

--- controllers/composed_controllers.js
+++ controllers/composed_controllers.js
@@ -201,84 +201,159 @@ const createInvoice = async (body, req) => {
             };
           }
         } else {
+          console.log("Is not electronic invoice");
           try {
-            const autorizacion = `${body.emizor.extras.facturaTicket}$${data.ack_ticket}`;
-            console.log("Autorizacion test", autorizacion);
-            console.log("Resp de la factura", data);
-            body.invoice.nroFactura = data.numeroFactura;
-            body.invoice.cuf = data.cuf;
-            body.invoice.autorizacion = autorizacion;
-            body.invoice.cufd = data.shortLink;
-            body.invoice.fechaEmision = data.fechaEmision;
-            try {
-              const invoiceCreated = await createInvoicePos(body.invoice);
-              console.log(
-                "Invoice created",
-                invoiceCreated.factura.rows[0].idFactura
-              );
-              body.venta.idFactura = invoiceCreated.factura.rows[0].idFactura;
+            const maxRetries = 50;
+            let retries = 0;
+            let stateData = null;
+            const delay = (ms) =>
+              new Promise((resolve) => setTimeout(resolve, ms));
+            while (retries < maxRetries) {
               try {
-                const saleCreated = await registerSalePos(
-                  body.venta,
-                  invoiceCreated.factura.rows[0].idFactura
+                const estadoFactura = await getEstadoFactura(
+                  req,
+                  data.ack_ticket
                 );
-                const ventaCreada = JSON.parse(saleCreated);
-                console.log("Sale created", ventaCreada);
-                try {
-                  const updatedLogs = await updateLogStockDetails(
-                    `NVAG-${ventaCreada.idCreado}`,
-                    idsCreados
-                  );
-                  return {
-                    code: 200,
-                    data: invoiceResponse,
-                    leyenda: JSON.parse(invoiceResponse).leyenda.descripcion,
-                    message: "Factura correcta",
-                  };
-                } catch (error) {
-                  return {
-                    code: 500,
-                    error: error,
-                    message: "Error al actualizar los logs",
-                  };
-                }
-              } catch {
+                console.log("Estado de la factura", estadoFactura);
+                stateData = JSON.parse(estadoFactura).data.data.estado;
+              } catch (error) {
+                console.log("Error", error);
+
                 return {
                   code: 500,
                   error: error,
-                  message: "Error al crear la venta",
+                  message: "Error al obtener el estado de la factura",
                 };
               }
-            } catch (error) {
-              try {
-                const stockBody = {
-                  accion: "add",
-                  idAlmacen: body.stock.idAlmacen,
-                  productos: body.stock.productos,
-                  detalle: `CVAGN-0`,
-                };
-                console.log("Stock body", stockBody);
-                const updatedStock = await updateProductStockPos(stockBody);
+              retries++;
+              await delay(3000); // Delay between retries
+              if (stateData === "VALIDA" || stateData === "RECHAZADA") {
+                const autorizacion = `${body.emizor.extras.facturaTicket}$${data.ack_ticket}`;
+                console.log("Autorizacion test", autorizacion);
+                if (stateData === "VALIDA") {
+                  try {
+                    const autorizacion = `${body.emizor.extras.facturaTicket}$${data.ack_ticket}`;
+                    console.log("Autorizacion test", autorizacion);
+                    console.log("Resp de la factura", data);
+                    body.invoice.nroFactura = data.numeroFactura;
+                    body.invoice.cuf = data.cuf;
+                    body.invoice.autorizacion = autorizacion;
+                    body.invoice.cufd = data.shortLink;
+                    body.invoice.fechaEmision = data.fechaEmision;
+                    try {
+                      const invoiceCreated = await createInvoicePos(body.invoice);
+                      console.log(
+                        "Invoice created",
+                        invoiceCreated.factura.rows[0].idFactura
+                      );
+                      body.venta.idFactura = invoiceCreated.factura.rows[0].idFactura;
+                      try {
+                        const saleCreated = await registerSalePos(
+                          body.venta,
+                          invoiceCreated.factura.rows[0].idFactura
+                        );
+                        const ventaCreada = JSON.parse(saleCreated);
+                        console.log("Sale created", ventaCreada);
+                        try {
+                          const updatedLogs = await updateLogStockDetails(
+                            `NVAG-${ventaCreada.idCreado}`,
+                            idsCreados
+                          );
+                          return {
+                            code: 200,
+                            data: invoiceResponse,
+                            leyenda: JSON.parse(invoiceResponse).leyenda.descripcion,
+                            message: "Factura correcta",
+                          };
+                        } catch (error) {
+                          return {
+                            code: 500,
+                            error: error,
+                            message: "Error al actualizar los logs",
+                          };
+                        }
+                      } catch {
+                        return {
+                          code: 500,
+                          error: error,
+                          message: "Error al crear la venta",
+                        };
+                      }
+                    } catch (error) {
+                      try {
+                        const stockBody = {
+                          accion: "add",
+                          idAlmacen: body.stock.idAlmacen,
+                          productos: body.stock.productos,
+                          detalle: `CVAGN-0`,
+                        };
+                        console.log("Stock body", stockBody);
+                        const updatedStock = await updateProductStockPos(stockBody);
+                        return {
+                          code: 500,
+                          error: error,
+                          message: "Error al crear la factura",
+                        };
+                      } catch (error) {
+                        return {
+                          code: 500,
+                          error: error,
+                          message: "Error al devolver el stock",
+                        };
+                      }
+                    }
+                  } catch (err) {
+                    return {
+                      code: 500,
+                      error: err,
+                      message: "Error en el proceso de facturacion",
+                    };
+                  }
+
+                } else {
+                  try {
+                    const stockBody = {
+                      accion: "add",
+                      idAlmacen: body.stock.idAlmacen,
+                      productos: body.stock.productos,
+                      detalle: `CVAGN-0`,
+                    };
+                    console.log("Stock body", stockBody);
+                    const updatedStock = await updateProductStockPos(
+                      stockBody
+                    );
+                    return {
+                      code: 500,
+                      error: "Factura rechazada",
+                      message: "Factura rechazada, intente nuevamente",
+                    };
+                  } catch (error) {
+                    return {
+                      code: 500,
+                      error: error,
+                      message: "Error al devolver el stock",
+                    };
+                  }
+
+                }
+              }
+              if (maxRetries === retries) {
                 return {
                   code: 500,
-                  error: error,
-                  message: "Error al crear la factura",
-                };
-              } catch (error) {
-                return {
-                  code: 500,
-                  error: error,
-                  message: "Error al devolver el stock",
+                  error: "Maximo de intentos alcanzado",
+                  message: "Maximo de intentos alcanzado",
                 };
               }
             }
-          } catch (err) {
+          } catch (error) {
             return {
               code: 500,
-              error: err,
-              message: "Error en el proceso de facturacion",
+              error: error,
+              message: "Error al obtener el estado de la factura",
             };
           }
+
+
         }
       } catch (error) {
         try {
@@ -320,6 +395,127 @@ const createInvoice = async (body, req) => {
     };
   }
 };
+
+
+const isValid = async () => {
+  if (stateData === "VALIDA" || stateData === "RECHAZADA") {
+    const autorizacion = `${body.emizor.extras.facturaTicket}$${data.ack_ticket}`;
+    if (stateData === "VALIDA") {
+      try {
+        body.invoice.nroFactura = data.numeroFactura;
+        body.invoice.cuf = data.cuf;
+        body.invoice.autorizacion = autorizacion;
+        body.invoice.cufd = data.shortLink;
+        body.invoice.fechaEmision = data.fechaEmision;
+        try {
+          const invoiceCreated = await createInvoicePos(
+            body.invoice
+          );
+          console.log(
+            "Invoice created",
+            invoiceCreated.factura.rows[0].idFactura
+          );
+          body.venta.idFactura =
+            invoiceCreated.factura.rows[0].idFactura;
+          try {
+            const saleCreated = await registerSalePos(
+              body.venta,
+              invoiceCreated.factura.rows[0].idFactura
+            );
+            const ventaCreada = JSON.parse(saleCreated);
+            try {
+              const updatedLogs = await updateLogStockDetails(
+                `NVAG-${ventaCreada.idCreado}`,
+                idsCreados
+              );
+              return {
+                code: 200,
+                data: invoiceResponse,
+                leyenda:
+                  JSON.parse(invoiceResponse).leyenda.descripcion,
+                message: "Factura correcta",
+              };
+            } catch (error) {
+              return {
+                code: 500,
+                error: error,
+                message: "Error al actualizar los logs",
+              };
+            }
+          } catch {
+            return {
+              code: 500,
+              error: error,
+              message: "Error al crear la venta",
+            };
+          }
+        } catch (error) {
+          try {
+            const stockBody = {
+              accion: "add",
+              idAlmacen: body.stock.idAlmacen,
+              productos: body.stock.productos,
+              detalle: `CVAGN-0`,
+            };
+            console.log("Stock body", stockBody);
+            const updatedStock = await updateProductStockPos(
+              stockBody
+            );
+            return {
+              code: 500,
+              error: error,
+              message: "Error al crear la factura",
+            };
+          } catch (error) {
+            return {
+              code: 500,
+              error: error,
+              message: "Error al devolver el stock",
+            };
+          }
+        }
+      } catch (err) {
+        return {
+          code: 500,
+          error: err,
+          message: "Error en el proceso de facturacion",
+        };
+      }
+    } else {
+      try {
+        const stockBody = {
+          accion: "add",
+          idAlmacen: body.stock.idAlmacen,
+          productos: body.stock.productos,
+          detalle: `CVAGN-0`,
+        };
+        console.log("Stock body", stockBody);
+        const updatedStock = await updateProductStockPos(
+          stockBody
+        );
+        return {
+          code: 500,
+          error: "Factura rechazada",
+          message: "Factura rechazada, intente nuevamente",
+        };
+      } catch (error) {
+        return {
+          code: 500,
+          error: error,
+          message: "Error al devolver el stock",
+        };
+      }
+
+    }
+  }
+  if (maxRetries === retries) {
+    return {
+      code: 500,
+      error: "Maximo de intentos alcanzado",
+      message: "Maximo de intentos alcanzado",
+    };
+  }
+}
 
 const createEmizorInvoice = async () => {
   return new Promise((resolve) =>
